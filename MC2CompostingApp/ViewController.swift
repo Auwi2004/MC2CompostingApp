b452a0578c721b9ba73e0671f19512297ba1c5b3
Auwi2004 <puriaprianty@yahoo.com>
2021-06-15 16:44:49 +0700

    Initial Commit

---
+++ MC2CompostingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC2CompostingApp
+//
+//  Created by Puri Aprianty on 15/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
